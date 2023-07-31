13541cb18941fb200e3008731b46cd3c79344011
Ishini Gamage <123579828+IshiniGamage@users.noreply.github.com>
2023-07-31 20:00:34 +0530

    Create insert_customer.php

---
+++ csquare/insert_customer.php
@@ -0,0 +1,24 @@
+<!-- insert_customer.php -->
+<?php
+include 'connection.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $title = $_POST["title"];
+    $first_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
+    $contact_number = $_POST["contact_number"];
+    $district = $_POST["district"];
+
+    // Insert customer data into the 'customers' table
+    $sql = "INSERT INTO customers (title, first_name, last_name, contact_number, district)
+            VALUES ('$title', '$first_name', '$last_name', '$contact_number', '$district')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Customer data inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
